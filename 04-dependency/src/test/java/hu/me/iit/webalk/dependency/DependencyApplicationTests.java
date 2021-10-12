38616d097209dfd3a74b2f42f3bd342a1ac11f0f
Zoltan Krizsan <krizsan@iit.uni-miskolc.hu>
2021-10-12 09:29:30 +0200

    dependency base

---
+++ 04-dependency/src/test/java/hu/me/iit/webalk/dependency/DependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.dependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
